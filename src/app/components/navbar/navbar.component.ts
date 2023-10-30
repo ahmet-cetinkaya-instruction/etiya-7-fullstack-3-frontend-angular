967ff45363760e26851441e204eed96455a6b542
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-10-30 17:11:50 +0300

    add bootstrap components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface NavItem {
+  label: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  title : string = "Rent A Car";
 
+  navItems : NavItem[] = [
+    { label: 'Home', route: '/home' },
+    { label: 'About', route: '/about' },
+    { label: 'Contact', route: '/contact' },
+  ];
 }
