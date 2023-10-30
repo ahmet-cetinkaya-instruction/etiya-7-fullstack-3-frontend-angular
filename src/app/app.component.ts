06a69b00fcec5a4cb267ba4d89aa3afa02b54a79
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-10-30 15:02:38 +0300

    add if else and for examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root></app-root>
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'RentACar';
+  title: string = 'Rent A Car'; // State
+
+  counter: number = 0;
+
+  brands: string[] = ['BMW', 'Mercedes', 'Audi', 'VW'];
+
+  increment(): void {
+    this.counter++;
+    this.brands.push('Tesla');
+  }
+
+  decrement(): void {
+    this.counter--;
+    this.brands.pop();
+  }
+
+  onIncrementButtonClicked(event: Event): void {
+    console.log(
+      '🚀 ~ file: app.component.ts:18 ~ AppComponent ~ onIncrementButtonClicked ~ event:',
+      event
+    );
+    this.increment();
+  }
+
+  onDecrementButtonClicked(event: Event): void {
+    console.log(
+      '🚀 ~ file: app.component.ts:25 ~ AppComponent ~ onDecrementButtonClicked ~ event:',
+      event
+    );
+    this.decrement();
+  }
 }
